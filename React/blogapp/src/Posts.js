95d014ea48788d3effa2185272ea7d4c07dd02c0
Shresth <shresthg6081@gmail.com>
2026-07-19 11:00:37 +0530

    ex3 and ex4

---
+++ React/blogapp/src/Posts.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Post from "./Post";
+
+class Posts extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    loadPosts() {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response.json())
+            .then(data => {
+                const posts = data.map(post =>
+                    new Post(post.id, post.title, post.body)
+                );
+
+                this.setState({
+                    posts: posts
+                });
+            });
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    componentDidCatch(error, info) {
+        alert(error);
+        console.error(info);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.posts.map(post => (
+                    <div key={post.id}>
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                        <hr />
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Posts;
